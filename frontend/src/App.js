f60d8b3e10e4bd5507f0b8ecd5d0b13596a2763d
mrocca1998 <mrocca1998@gmail.com>
2021-08-15 22:34:09 -0400

    react add bootstrap express alter models

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" 
+      integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" 
+      crossorigin="anonymous"/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
