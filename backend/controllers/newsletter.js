2f007e2b6ef6adf455eeb69b2a4ff40faa9ac431
mrocca1998 <mrocca1998@gmail.com>
2021-08-19 18:14:33 -0400

    implemented search api

--- backend/controllers/newsletter.js
+++ backend/controllers/newsletter.js
@@ -1,19 +1,51 @@
 const nodemailer = require("nodemailer");
-var html_to_pdf = require('html-pdf-node');
-const models = require('../models')
+const html_to_pdf = require('html-pdf-node');
+const models = require('../models');
+const SerpApi = require('google-search-results-nodejs')
 
 exports.generateNewsletter = async function (user, date) {
-    let html = '<p>hi</p><p>hey</p><a href="https://www.w3schools.com">Click Me</a>'
-    let file = { content: html, };
-    let options = { path: './newsletters/' + user + date + '.pdf'}
+  let search = new SerpApi.GoogleSearch("insert key here")
+  models.Query.findAll().then(queries => {
+    let html = '<div style="text-align: center; font-family: Arial"><h1>HAPPY SHOPPING!</h1>'
+    let j = 0
+    queries.forEach(query => {
+      search.json({
+        tbm: 'shop',
+        q: query.text, 
+       }, (results) => {
+        html += '<h2>' + query.text + '</h2>'
+        let listings = results['shopping_results']
+        for (let i = 0; i < parseInt(query.quantity); i++) {
+          if (i < listings.length) {
+            let listing = listings[i];
+            html += '<img src="' + listing['thumbnail'] + '" alt="' + query.text + '" height = "200px" width = "200px" />'
+            html += '<br/><br/>'
+            html += '<a href="' + listing['link'] + '">' + listing['title'] + '</a>'
+            html += '<p>' + 'Price: ' + listing['price'] + '</p>'
+            html += '<br/>'
+          }
+        }
+        j++
+        if (j == queries.length) {
+          html += '</div>'
+          generatePDF(user, date, html)
+        }
+       })
+    })
+  })
+}
 
-    try {
-        await html_to_pdf.generatePdf(file, options)
-        .then(sendEmail(user, date))
-    } catch(e) {
-        console.log(e)
-    }
+async function generatePDF(user, date, html) {
+  let file = { content: html, };
+  console.log(html)
+  let options = { path: './newsletters/' + user + date + '.pdf'}
 
+  try {
+      await html_to_pdf.generatePdf(file, options)
+      .then(sendEmail(user, date))
+  } catch(e) {
+      console.log(e)
+  }
 }
 
 function sendEmail(user, date) {
